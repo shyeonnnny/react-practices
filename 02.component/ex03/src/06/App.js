c3d18262a3e9acdfe0c1613a309ba54662b5283b
shyeonnnny <ktmdgus970807@gmail.com>
2021-12-01 09:46:49 +0900

    2021.12.01

--- 02.component/ex03/src/06/App.js
+++ 02.component/ex03/src/06/App.js
@@ -2,14 +2,23 @@ import React, {Component, useRef} from 'react';
 import './assets/scss/App.scss';
 
 export default class App extends Component {
+    
+    // 함수형 컴포넌트에서는 this.current.outter.scrollTop이라고 써야하지만
+    // 클래스형 컴포넌트에서는 current를 떼야한다
 
+    onScroll(e){
+        console.log(this.outterRef.scrollTop, ':' ,this.outterRef.clientHeight, ";", this.innerRef.clientHeight)
+    }
 
     render() {
         return (
             <div
-                className={'App'}>
+                ref={ (ref) => this.outterRef = ref }
+                className={'App'}
+                onScroll={ this.onScroll.bind(this)}>
                 <div
-                    ref={ (ref) => this.innerRef = ref }>
+                    ref={ ref => this.innerRef = ref }>
+
                     <ul>
                         {Array.from({length: 100}, (_, i) => i + 1).map(i =>
                             <li>
