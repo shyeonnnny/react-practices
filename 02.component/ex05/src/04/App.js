8d741d27bb9b1653a5f620decb22f94b07122100
shyeonnnny <ktmdgus970807@gmail.com>
2021-12-06 12:15:41 +0900

    2021.12.06

--- 02.component/ex05/src/04/App.js
+++ 02.component/ex05/src/04/App.js
@@ -1,15 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+    const [getCurrentClockTiem,setCurrentClock] = useState(getCurrentClockTiem());
+    const [ticks, setTicks]  =useState(0);
+    
+    
+    useEffect(() =>{
+        setInterval(function(){
+            setCurrentClock(getCurrentClockTiem());
+            setTicks(ticks + 1);
+        }, 1000);
+    }, []);
 
+
+    // useEffect(() =>{
+    //     console.log("-----------------");
+    //     setInterval(function(){
+    //         setCurrentClock(getCurrentClockTiem());
+    //         setTicks(tick+1);
+    //     }, 1000);
+    // }, [currentClockTime]);
+
+
+
+
+    useEffect(() => {
+        console.log("-----------------------");
+
+    })
 
     return (
+        <Fragment>
+            <span>{ticks}</span>
             <Clock
                 message={'ex05: useEffect Hook example'}
                 hours={state.hours}
                 minutes={state.minutes}
                 seconds={state.seconds}/>
+        </Fragment>
     );
 }
